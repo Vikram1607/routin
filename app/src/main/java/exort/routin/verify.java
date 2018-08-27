31fe3d3919e1a0d3946871f7c9d702778ddb38e0
Vikram1607 <vikrammano8@gmail.com>
2018-08-27 23:32:17 +0530

    first commit

---
+++ app/src/main/java/exort/routin/verify.java
@@ -0,0 +1,13 @@
+package exort.routin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class verify extends AppCompatActivity {
+0
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+    }
+}
