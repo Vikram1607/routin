93f31d418e01eb7643b8597baa336aa3255b517a
Vikram1607 <vikrammano8@gmail.com>
2018-08-28 11:43:13 +0530

    Second Commit

--- app/src/main/java/exort/routin/verify.java
+++ app/src/main/java/exort/routin/verify.java
@@ -1,13 +1,27 @@
 package exort.routin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class verify extends AppCompatActivity {
-0
+
+    ImageButton arrow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
+        arrow=(ImageButton)findViewById(R.id.imageButton);
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"You will receive an OTP",Toast.LENGTH_SHORT).show();
+                Intent h=new Intent(verify.this,otp.class);
+                startActivity(h);
+            }
+        });
     }
 }
